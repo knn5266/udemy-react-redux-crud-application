9f3c4f113cc83b204400dd906a8658e6689a4906
Kanon <mkc35r.0@gmail.com>
2024-03-31 20:26:23 +0900

    implement event new component

--- app/src/index.js
+++ app/src/index.js
@@ -3,17 +3,24 @@ import ReactDOM from 'react-dom';
 import { legacy_createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { thunk } from 'redux-thunk';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './index.css';
 import Reducer from './reducers';
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import reportWebVitals from './reportWebVitals';
 
 const store = legacy_createStore(Reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/events/new" Component={EventsNew} />
+        <Route exact path="/" element={<EventsIndex></EventsIndex>}></Route>
+      </Routes>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
