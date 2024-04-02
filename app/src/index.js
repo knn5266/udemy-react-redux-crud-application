bfa30522360298a8503a73fdddbf51de98e94fce
Kanon <mkc35r.0@gmail.com>
2024-04-02 17:22:29 +0900

    material-ui events_index.js

--- app/src/index.js
+++ app/src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { thunk } from 'redux-thunk';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import muiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import './index.css';
 import reducer from './reducers';
@@ -12,6 +13,7 @@ import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import EventsShow from './components/events_show';
 import reportWebVitals from './reportWebVitals';
+import { MuiThemeProvider } from 'material-ui/styles';
 
 const enhancer =
   process.env.NODE_ENV === 'development'
@@ -19,16 +21,20 @@ const enhancer =
     : applyMiddleware(thunk);
 const store = legacy_createStore(reducer, enhancer);
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/events/new" element={<EventsNew />} />
-        <Route path="/events/:id" element={<EventsShow />} />
-        <Route exact path="/" element={<EventsIndex />} />
-        <Route exact path="/events" element={<EventsIndex />} />
-      </Routes>
-    </BrowserRouter>
-  </Provider>,
+  <MuiThemeProvider>
+    {' '}
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/events/new" element={<EventsNew />} />
+          <Route path="/events/:id" element={<EventsShow />} />
+          <Route exact path="/" element={<EventsIndex />} />
+          <Route exact path="/events" element={<EventsIndex />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </MuiThemeProvider>,
+
   document.getElementById('root')
 );
 reportWebVitals();
