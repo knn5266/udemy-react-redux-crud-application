0f0580ce7d6e950e3192edb72e0e23bd34e011e1
Kanon <mkc35r.0@gmail.com>
2024-04-02 11:31:09 +0900

    error in events-show

--- app/src/index.js
+++ app/src/index.js
@@ -4,14 +4,19 @@ import { legacy_createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { thunk } from 'redux-thunk';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import './index.css';
-import Reducer from './reducers';
+import reducer from './reducers';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
 import reportWebVitals from './reportWebVitals';
 
-const store = legacy_createStore(Reducer, applyMiddleware(thunk));
+const enhancer =
+  process.env.NODE_ENV === 'development'
+    ? composeWithDevTools(applyMiddleware(thunk))
+    : applyMiddleware(thunk);
+const store = legacy_createStore(reducer, enhancer);
 
 ReactDOM.render(
   <Provider store={store}>
